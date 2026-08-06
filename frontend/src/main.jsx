565be1fdc423e652dc1f58498ea409cafaf4a53b
pubgsufian-afk <pubg.sufian@gmail.com>
2026-08-06 11:23:40 +0200

    feat: mount one unified portal application

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,7 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
+import { createRoot } from 'react-dom/client'
+import App from './App.jsx'
 import './styles.css'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-)
+const root = document.getElementById('root')
+if (!root) throw new Error('Portal-Wurzelelement fehlt.')
+createRoot(root).render(<App />)
